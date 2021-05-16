8bae1f56efcb7bb5cb0164040079a809124d48ed
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-16 13:33:11 -0400

    added assembly info

--- XRTK.ProjectName/Packages/com.xrtk.projectname/Runtime/AssemblyInfo.cs
+++ XRTK.ProjectName/Packages/com.xrtk.projectname/Runtime/AssemblyInfo.cs
@@ -2,3 +2,10 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_PROJECTNAME
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyTitle("com.xrtk.projectname")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
